bd442ffa2539c2f3b1846f4645b54d161b46239c
Michael Zabka <zabka@avantcore.cz>
2013-06-03 19:00:25 +0200

    view no other detail

--- zapa-tracking/lib/views.js
+++ zapa-tracking/lib/views.js
@@ -538,7 +538,7 @@ exports.views = {
             if(isBot) return;
 
             // ignore other than detail, default and category
-            if (action !== 'Site:detail' && action !== 'Site:default' && action !== 'Site:category' && action !== 'Site:order' && action !== 'Site:complete') return; // && action !== 'Site:order' && action !== 'Site:complete'
+            if (['Site:detail', 'Site:default', 'Site:category', 'Site:order', 'Site:complete'].indexOf(action) === -1) return;
 
             // -- emit --
             // emit bot action
