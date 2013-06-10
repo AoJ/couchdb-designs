f57ef690a9dc7df8cee4ecf051583e817a3cee56
AoJ on F <aoj@n13.cz>
2013-06-10 11:40:33 +0200

    +feed list deals

--- design-feed/lib/views.js
+++ design-feed/lib/views.js
@@ -113,6 +113,49 @@ exports.lists = {
 
 
     //todo
+    "display-table": function(head, req) {
+
+        //todo
+
+        //http://d.n13.cz:9090/feed-watchman/_design/roll/_list/deal-progress/counts-by-deal?startkey=[%22berslevu%22,%222866%22]&endkey=[%22berslevu%22,%222866%22,{}]
+        //http://d.n13.cz:9090/feed-watchman/_design/roll/_list/deal-progress/counts-by-deal?startkey=[%22berslevu%22,%223002%22]&endkey=[%22berslevu%22,%223002%22,{}]
+        //http://couch.allin1.cz:9090/feed-watchman/_design/roll/_list/deal-progress/counts-by-deal?startkey=[%22berslevu%22,%222866%22]&endkey=[%22berslevu%22,%222866%22,{}]&stale=ok
+
+        var handlebars = require('handlebars');
+        var data = [];
+        var row;
+
+        var options = {
+            title: req.query.title,
+            data: []
+        }
+
+        provides('html', function() {
+
+            while (row = getRow()) {
+                
+                //header
+                if(data.length === 0) {
+                    var columns = [];
+                    for(var p = 0; p < row.key.length; p++) columns.push(p);
+                    columns.push('value'); //vytvoří počet prvků podle počtu key
+                    data.push('<tr><th>'+(columns.join('</th><th>'))+'</th></tr>');
+                }
+
+                data.push('<tr><td>'+(row.key.join('</td><td>'))+'</td><td>'+row.value+'</td></tr>');
+
+                last = row;
+            }
+
+            options.data = data.join("\n");
+            var html = handlebars.templates['table_simple.tpl'](options);
+
+            return html;
+        })
+    },
+
+
+    //todo
     "deal-table": function(head, req) {
 
         //todo
@@ -133,6 +176,7 @@ exports.lists = {
             labelY: req.query.y,
             title: req.query.title,
             keySlice: req.query.keySlice || -1,
+            all: req.query.all,
             data1: [],
             data2: [],
             data: []
@@ -144,14 +188,14 @@ exports.lists = {
             while (row = getRow()) {
                 if (!first) {
                     first = row;
-                    last = row
+                    last = row;
                 }
 
                 var date = row.value.date.split(' ');
                 var hour = date[1].split(':')[0];
 
                 //pošleme dál pouze data, kdy se něco změnilo
-                if(last.value.total_price === row.value.total_price ) continue;
+                //if(!options.all) if(last.value.total_price === row.value.total_price ) continue;
 
                 var relativeCustomers = row.value.customers - first.value.customers;
                 var relativePrice = row.value.total_price - first.value.total_price;
@@ -184,18 +228,27 @@ exports.views = {
             var regex = /^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/;
             var parts = doc.meta.time.replace(regex, "$1 $2 $3 $4 $5 $6").split(' ');
 
+            function getValue(value) {
+                if(!!value.indexOf && value.length == 1) {
+                    if(typeof value[0] == 'Object') return value;
+                    else return value[0];
+                }
+                return value;
+            }
+            var deals = doc.DEAL || doc.SERVER.DEAL || [];
 
-            for (var i = 0; i < doc.DEAL.length; i++) {
-                var deal = doc.DEAL[i];
-                var key = [doc.meta.name, deal.ID];
+
+            for (var i = 0; i < deals.length; i++) {
+                var deal = deals[i];
+                var key = [doc.meta.name, getValue(deal.ID)];
                 var docKey = key;
 
                 var data = {
-                    customers: parseInt(deal.CUSTOMERS, 10),
+                    customers: parseInt(getValue(deal.CUSTOMERS), 10),
                     total_price: null,
-                    final_price: parseFloat(deal.FINAL_PRICE, 10),
-                    discount: parseFloat(deal.DISCOUNT, 10),
-                    id: deal.ID,
+                    final_price: parseFloat(getValue(deal.FINAL_PRICE), 10),
+                    discount: parseFloat(getValue(deal.DISCOUNT), 10),
+                    id: getValue(deal.ID),
                     date: doc.meta.time
                 };
                 data.total_price = data.customers * data.final_price;
@@ -213,18 +266,29 @@ exports.views = {
             var regex = /^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/;
             var parts = doc.meta.time.replace(regex, "$1 $2 $3 $4 $5 $6").split(' ');
 
+
+            function getValue(value) {
+                if(!!value.indexOf && value.length == 1) {
+                    if(typeof value[0] == 'Object') return value;
+                    else return value[0];
+                }
+                return value;
+            }
+            var deals = doc.DEAL || doc.SERVER.DEAL || [];
+
+
             ["total", "customers"].forEach(function(type) {
-                for (var i = 0; i < doc.DEAL.length; i++) {
-                    var deal = doc.DEAL[i];
-                    var key = [doc.meta.name, type, deal.ID];
+                for (var i = 0; i < deals.length; i++) {
+                    var deal = deals[i];
+                    var key = [doc.meta.name, type, getValue(deal.ID)];
                     var docKey = key;
 
                     var data = {
-                        customers: parseInt(deal.CUSTOMERS, 10),
+                        customers: parseInt(getValue(deal.CUSTOMERS), 10),
                         total_price: null,
-                        final_price: parseFloat(deal.FINAL_PRICE, 10),
-                        discount: parseFloat(deal.DISCOUNT, 10),
-                        id: deal.ID,
+                        final_price: parseFloat(getValue(deal.FINAL_PRICE), 10),
+                        discount: parseFloat(getValue(deal.DISCOUNT), 10),
+                        id: getValue(deal.ID),
                         date: doc.meta.time
                     };
                     data.total_price = data.customers * data.final_price;
@@ -246,25 +310,35 @@ exports.views = {
             var regex = /^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/;
             var parts = doc.meta.time.replace(regex, "$1 $2 $3 $4 $5 $6").split(' ');
 
+
+            function getValue(value) {
+                if(!!value.indexOf && value.length == 1) {
+                    if(typeof value[0] == 'Object') return value;
+                    else return value[0];
+                }
+                return value;
+            }
+            var deals = doc.DEAL || doc.SERVER.DEAL || [];
+
             //too much records
             return;
 
-            for (var i = 0; i < doc.DEAL.length; i++) {
-                var deal = doc.DEAL[i];
-                var key = [doc.meta.name, deal.DEAL_START, deal.ID];
+            for (var i = 0; i < deals.length; i++) {
+                var deal = deals[i];
+                var key = [doc.meta.name, getValue(deal.DEAL_START), getValue(deal.ID)];
                 var docKey = key;
 
                 var data = {
-                    customers: parseInt(deal.CUSTOMERS, 10),
+                    customers: parseInt(getValue(deal.CUSTOMERS), 10),
                     total_price: null,
-                    final_price: parseFloat(deal.FINAL_PRICE, 10),
-                    discount: parseFloat(deal.DISCOUNT, 10),
-                    id: deal.ID,
+                    final_price: parseFloat(getValue(deal.FINAL_PRICE), 10),
+                    discount: parseFloat(getValue(deal.DISCOUNT), 10),
+                    id: getValue(deal.ID),
                     date: doc.meta.time
                 };
                 data.total_price = data.customers * data.final_price;
 
-                emit(key.concat([deal.TITLE, deal.URL]), 1);
+                emit(key.concat([getValue(deal.TITLE), getValue(deal.URL)]), 1);
             }
         },
         reduce: "_count"
