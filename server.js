e682075e85d492f9c67838ee9ca844693c82e4b5
AoJ on F <aoj@n13.cz>
2013-06-25 11:44:25 +0200

    production

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ var axon = require('axon');
 var emitter = axon.socket('pub-emitter');
 var nano = require('nano');
 var _ = require('underscore');
-var appUp = require('appUp');
+var util = require('util');
 
 axon.codec.define('text', {
   encode: function(msg) { return new Buffer(msg); },
   decode: function(msg) { return msg.toString(); }
 });
-var collector = axon.socket('sub');
+var collector = axon.socket('sub-ext');
 collector.set('identity', 'collector');
-collector.set('retry max timeout', 60 * 1000);
-
+collector.set('retry max timeout', 5 * 60 * 1000);
 
 
 var settings = {
-  listenPort: process.env.PORT || process.argv[2] || 3000,
+  listenPort: parseInt(process.env.PORT || process.argv[2] || 3000, 10),
   broadcastPort: 10001,
   broadcastHost: '127.0.0.1',
   couchDsn: 'http://zapa:59QDnxudRxFp@itchy.allin1.cz:9090',
@@ -32,20 +31,24 @@ var couch = couchNed.use(settings.couchDb);
 
 
 collector.bind(settings.listenPort, function(){
-  console.log("server running at port " + settings.listenPort +"\n");
+  if(process.send) process.send('online');
+  util.log("server running at port " + settings.listenPort +"\n");
 });
 
-collector.on('message', function(project, group, type, title, desc, content, meta){
-  broadcast(project, group, type, title, desc, content, meta);
+collector.on('message', function(sock, project, group, type, title, desc, content, meta){
+  meta.bytesRead = sock.bytesRead;
+  meta.bytesWritten = sock.bytesWritten;
+  broadcast(sock.remoteAddress, project, group, type, title, desc, content, meta);
 });
 
 
 
 
 emitter.bind(settings.broadcastPort, settings.broadcastHost);
-function broadcast(project, group, type, title, desc, content, meta) {
-  console.log(project + ":" + group + ":" + type + " - " + title);
-  emitter.emit(project + ":" + group + ":" + type, {title: title, desc: desc, meta: meta, data: content});
+function broadcast(ip, project, group, type, title, desc, content, meta) {
+  var msg = project + ":" + group + ":" + type;
+  var data = {title: title, desc: desc, meta: meta, ip: ip, data: content};
+  emitter.emit(msg, data);
 }
 
 var sock = axon.socket('sub-emitter');
@@ -53,13 +56,15 @@ sock.connect(settings.broadcastPort, settings.broadcastHost);
 
 sock.on('zapakatel:*:tracking', function(group, data){
 
+  var time = (new Date);
   var data = {
     "type": ['/type/access'],
-    "time": (new Date).toISOString(),
+    "time": time.toISOString(),
+    "timestamp": time.getTime(),
     "duration": data.meta.timetrace,
     "group": group,
     "project": "zapakatel",
-    "ip": null,
+    "ip": data.ip,
     "message": data.title,
     "data": data.data
   };
@@ -84,6 +89,16 @@ sock.on('zapakatel:*:tracking', function(group, data){
       });
     }
 
+    //blacklist sentHeaders
+    var blackListHeaders = ["X-Powered-By", "Expires", "Cache-Control", "Pragma", "X-Frame-Options"];
+    if(data.data.sentHeaders) {
+      data.data.sentHeaders = _.filter(_.values(data.data.sentHeaders), function(record) {
+        return _.all(blackListHeaders, function(blacklist){
+          return record.indexOf(blacklist) === -1;
+        });
+      });
+    }
+
    couch.insert(data, function (e, doc) {
         if(e) emitter.emit("sys:couchdb:error:tracking", {e: e, doc: doc, data: data});
         else emitter.emit("sys:couchdb:insert:tracking", doc);
@@ -93,7 +108,6 @@ sock.on('zapakatel:*:tracking', function(group, data){
 
 sock.on('zapakatel:dev:*', function(type, data){
   if(type === 'tracking') return;
-  //console.log(arguments, type);
 });
 
 
@@ -105,3 +119,21 @@ sock.on('sys:couchdb:insert:tracking', function(doc){
 sock.on('sys:couchdb:error:tracking', function(data){
   console.error(data.e);
 })
+
+
+
+process.on('message', function(message) {
+ if (message === 'shutdown') {
+   sock.close();
+   collector.close();
+   process.exit(0);
+   setTimeout(function(){
+    process.exit(1);
+   }, 2000);
+ }
+});
+
+
+process.on('uncaughtException', function(err) {
+  console.error(process.pid + " " + err.stack);
+});
